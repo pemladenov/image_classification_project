dc9a8c016a9a2733c1b9f4ef3a0651628c090eda
501a3594-8e42-4455-8bbc-35f968b37e68 <NDgxNjUyMDE2ODUwOiHbsgZ9LlpX9i126aSk+dBA6WQ6>
2023-03-21 15:13:32 +0100

    bootstrap and some refactoring

--- app.py
+++ app.py
@@ -31,29 +31,30 @@ def index():
 def predict():
     img_bytes = request.files['image'].read()
     img = Image.open(io.BytesIO(img_bytes))
+    img = img.convert('RGB')
     img = img.resize((224, 224))
-    img_arr = np.array(img) / 255.0
-    img_arr = img_arr.astype(np.float32)
-    # Assume img_arr has shape (height, width, channels)
+    img_arr = np.array(img).astype(np.float32)
+    
+    mean = np.array([0.485, 0.456, 0.406], dtype=np.float32)
+    std = np.array([0.229, 0.224, 0.225], dtype=np.float32)
+    img_arr = img_arr / 255.0  # convert pixel values to the range [0, 1]
+    img_arr = (img_arr - mean) / std  # normalize using the provided mean and std
+    
     img_arr = np.expand_dims(img_arr, axis=0)  # Add a batch dimension
     img_arr = np.transpose(img_arr, (0, 3, 1, 2))  # Transpose to (batch_size, channels, height, width)
+    img_arr = img_arr[:, ::-1, :, :]  # Convert from RGB to BGR
+    
     output = inference.run(None, {'data_0': img_arr})
-
 
     # make predictions
-    output = inference.run(None, {'data_0': img_arr})
     probs = np.squeeze(output)
-
-    # get the top 5 predicted classes and their probabilities
     top_indices = np.argsort(probs)[::-1][:5]
     top_probs = probs[top_indices]
     top_classes = [class_dict[i] for i in top_indices]
 
     # return the result as a JSON object
-    result = {'class': top_classes[0], 'probability': str(top_probs[0])}
-    return jsonify(result)
-
-
+    results = [{'class': cls, 'probability': str(prob)} for cls, prob in zip(top_classes, top_probs)]
+    return jsonify(results)
     
 if __name__ == '__main__':
     app.run()
