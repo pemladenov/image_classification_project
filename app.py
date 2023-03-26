d9e04096b05b66901aa452b00b1c3368ed9dfde9
501a3594-8e42-4455-8bbc-35f968b37e68 <NDgxNjUyMDE2ODUwOiHbsgZ9LlpX9i126aSk+dBA6WQ6>
2023-03-26 16:43:14 +0200

    git lfs problems - clone project and push new

--- app.py
+++ app.py
@@ -6,19 +6,17 @@ from flask_bootstrap import Bootstrap
 from PIL import Image
 from flask import Flask, render_template, request, jsonify
 
-
 app = Flask(__name__)
 Bootstrap(app)
 
-inference = onnxruntime.InferenceSession('squeezenet1.0-12-int8.onnx', providers=['CPUExecutionProvider'])
+inference = onnxruntime.InferenceSession('bvlcalexnet-12.onnx', providers=['CPUExecutionProvider'])
 
 # Load the model
-model = onnx.load('squeezenet1.0-12-int8.onnx')
+model = onnx.load('bvlcalexnet-12.onnx')
 
 # Get the input node (the first node of the graph)
-input_name = model.graph.input[0].name
+input_name = inference.get_inputs()[0].name
 print(input_name)
-
 
 with open('static/synset.txt', 'r') as f:
     class_dict = [line.strip() for line in f.readlines()]
@@ -31,21 +29,19 @@ def index():
 def predict():
     img_bytes = request.files['image'].read()
     img = Image.open(io.BytesIO(img_bytes))
-    img = img.convert('RGB')
-    img = img.resize((224, 224))
+    img = img.convert('RGB')    
+    img = img.resize((224, 224))  # Resize the image to the expected input size for AlexNet
     img_arr = np.array(img).astype(np.float32)
-    
+
     mean = np.array([0.485, 0.456, 0.406], dtype=np.float32)
     std = np.array([0.229, 0.224, 0.225], dtype=np.float32)
     img_arr = img_arr / 255.0  # convert pixel values to the range [0, 1]
     img_arr = (img_arr - mean) / std  # normalize using the provided mean and std
-    
+
     img_arr = np.expand_dims(img_arr, axis=0)  # Add a batch dimension
     img_arr = np.transpose(img_arr, (0, 3, 1, 2))  # Transpose to (batch_size, channels, height, width)
-    img_arr = img_arr[:, ::-1, :, :]  # Convert from RGB to BGR
-    
-    output = inference.run(None, {'data_0': img_arr})
 
+    output = inference.run(None, {"data_0": img_arr})
     # make predictions
     probs = np.squeeze(output)
     top_indices = np.argsort(probs)[::-1][:5]
@@ -55,6 +51,5 @@ def predict():
     # return the result as a JSON object
     results = [{'class': cls, 'probability': str(prob)} for cls, prob in zip(top_classes, top_probs)]
     return jsonify(results)
-    
 if __name__ == '__main__':
     app.run()
