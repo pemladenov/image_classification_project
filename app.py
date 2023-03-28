ecd9e479ab49410a07483ec09aaac684a91dec2f
501a3594-8e42-4455-8bbc-35f968b37e68 <NDgxNjUyMDE2ODUwOiHbsgZ9LlpX9i126aSk+dBA6WQ6>
2023-03-28 11:17:10 +0200

    port changed to 8000

--- app.py
+++ app.py
@@ -58,5 +58,6 @@ def predict():
     return jsonify(results)
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+  #  from waitress import serve
+  #  serve(app, host="0.0.0.0", port=8000)
+  app.run(host='0.0.0.0', port=8000)
